a638c3e75d85ed60e6ab62f973b3bbaa0eaa73a1
Jason Cozens <anton.cozens@gmail.com>
2022-03-02 11:41:16 +0000

    Added image

---
+++ christmastree.py
@@ -0,0 +1,3 @@
+height = 25
+for h in list(range(0, 20)):
+    print(" " * (20 - h) + ('*' * 2 * h))
--- course_1/hello.py
+++ course_1/hello.py
@@ -1,2 +1,6 @@
-name = input('Enter name: ')
-print('hello ' + name)
+print('Enter names')
+name = ''
+while name != 'Q':
+    name = input('Enter name: ')
+    print('hello ' + name)
+    
